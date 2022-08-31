3a8235cbf1910022382fb94eaaef02bf490667e8
slydragonn <alogo18m@gmail.com>
2022-08-31 17:06:05 -0500

    initial commit

---
+++ helpers/comments.ts
@@ -0,0 +1,22 @@
+const comments = [
+  {
+  commentId: "comment7654987",
+  userId: "user4567",
+  username: "alogo",
+  comment: "hello world",
+  creationDate: "12-08-2021",
+  likes: 123,
+  dislikes: 3,
+  },
+  {
+    commentId: "comment1234567",
+    userId: "user1234",
+    username: "hello",
+    comment: "HI!",
+    creationDate: "2-06-2020",
+    likes: 123,
+    dislikes: 3,
+  }
+]
+
+export default comments
